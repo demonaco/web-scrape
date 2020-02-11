ae324070d7e0fe7a430f11fd59c4d42573b706cb
Matt DeMonaco <mdemonaco@gmail.com>
2020-02-10 21:42:20 -0800

    Change in file/folder structure.

--- routing/scrape.js
+++ routing/scrape.js
@@ -1,36 +1,3 @@
-var express = require('express');
-
-var logger = require('morgan');
-
-var mongoose = require('mongoose');
-
-var cheerio = require('cheerio');
-
-var axios = require('axios');
-
-var db = require("./models");
-
-var PORT = 8000;
-
-var app = express();
-
-app.use(logger("dev"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//Setting up our handlebars engine
-var exphbs = require('express-handlebars');
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-app.use(express.static("public"));
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
-
-
 //Using a get route to grab HTTP for scraping
 app.get("/", function (req, res) {
     axios.get("https://www.wsj.com").then(function (response) {
@@ -95,9 +62,4 @@ app.post("/articles/:id", function(req, res) {
     .catch(function(err) {
         res.json(err);
     });
-});
-
-//Server info
-app.listen(PORT, function() {
-    console.log(PORT + " is running")
 });
--- server.js
+++ server.js
@@ -10,6 +10,10 @@ var axios = require('axios');
 
 var db = require("./models");
 
+var path = require('path');
+
+var Router = require('router');
+
 var PORT = 8000;
 
 var app = express();
@@ -26,9 +30,79 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+
+
+//Using a get route to grab HTTP for scraping
+app.get("/", function (req, res) {
+    axios.get("https://www.wsj.com").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+
+        $("article h3").each(function (i, element) {
+
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            
+            db.Article.create(result)
+                .then(function (articles) {
+                    console.log(articles);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+
+        res.render("index");
+    });
+});
+//go to page to locate articles
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+    .then(function(articles) {
+        res.json(articles)
+    }).catch(function(err) {
+        res.json(err);
+    });
+});
+//grab a specific article by its ID
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(articles) {
+        res.json(articles);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+//posting and saving an article
+
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(articles) {
+        res.json(articles);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
 
 
 //Server info
